98f325d5fcbab3ef2298b2be37dd957adb81666d
mejialuismiguel <mejia_luismiguel@hotmail.com>
2025-06-02 10:54:36 -0500

    atriuto related, me permite traer campos de modelos relacionados, recuerda quitar el readonly y colocar store si quieres guardar datos en ese campo relacionado del modelo local.

--- custom_addons/grades_manager/models/grades_course.py
+++ custom_addons/grades_manager/models/grades_course.py
@@ -25,6 +25,7 @@ class GradesCourse(models.Model):
     course_image = fields.Binary(string='Course icon')
     course_shift = fields.Selection([('day', 'Day'),('night', 'Night')], string='Course shift')
     teacher_id = fields.Many2one('res.partner', string='Teacher', domain=[('is_teacher', '=', True)], default=_get_default_teacher, ondelete='restrict')
+    teacher_email = fields.Char(related='teacher_id.email', string='Teacher email', store=True)
     evaluation_ids = fields.One2many('grades.evaluation', 'course_id', string='Evaluations')
     student_ids = fields.Many2many('res.partner','grades_course_students_rel', string='Students')
     state = fields.Selection([('register', 'Register'),('in_progress', 'In Progress'), ('finished', 'Finished')], String = 'State', default='register')
