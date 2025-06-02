5b89f97aadbf24b049c9b8b747dff4a0c13c6157
mejialuismiguel <mejia_luismiguel@hotmail.com>
2025-06-02 16:33:29 -0500

    funcion create ejecutar acciones de creacion de registros

--- custom_addons/grades_manager/models/grades_evaluation.py
+++ custom_addons/grades_manager/models/grades_evaluation.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class GradesEvaluation(models.Model):
     _name = 'grades.evaluation'
@@ -9,3 +9,16 @@ class GradesEvaluation(models.Model):
     observations = fields.Text(string='Observations')
     course_id = fields.Many2one('grades.course', string='Course', ondelete='cascade')
     grade_ids = fields.One2many('grades.grade', 'evaluation_id', string='Grades')
+
+    @api.model_create_multi
+    def create(self, vals):
+        result = super(GradesEvaluation, self).create(vals)
+        for student in result.course_id.student_ids:
+            if not self.env['grades.grade'].search([('student_id', '=', student.id), ('evaluation_id', '=', result.id)]):
+                self.env['grades.grade'].create({
+                    'student_id': student.id,
+                    'value': 0,
+                    'date': fields.Date.today(),
+                    'evaluation_id': result.id,
+                })
+        return result
