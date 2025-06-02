81f667c0aa4ac346e789d9aab7ddd59dbb309fd9
mejialuismiguel <mejia_luismiguel@hotmail.com>
2025-06-02 10:07:00 -0500

    campos default, defaul querys ORM

--- custom_addons/grades_manager/models/grades_course.py
+++ custom_addons/grades_manager/models/grades_course.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from reportlab.graphics.shapes import String
 
 from odoo import models, fields
@@ -9,17 +11,20 @@ class GradesCourse(models.Model):
     _order = 'name'
     _rec_name = 'name'
 
+    def _get_default_teacher(self):
+        return self.env['res.partner'].search([('is_teacher', '=', True)], limit=1).id
+
     name = fields.Char(string='Name')
     student_qty = fields.Integer(string='Student quantity')
     grades_averages = fields.Float(string='Grades averages')
     description = fields.Text(string='Description')
     is_active = fields.Boolean(string='Active', default=True)
-    course_start = fields.Date(string='Course start')
+    course_start = fields.Date(string='Course start', default=fields.Date.today())
     course_end = fields.Date(string='Course end')
     last_evaluation = fields.Datetime(string='Last evaluation')
     course_image = fields.Binary(string='Course icon')
     course_shift = fields.Selection([('day', 'Day'),('night', 'Night')], string='Course shift')
-    teacher_id = fields.Many2one('res.partner', string='Teacher', domain=[('is_teacher', '=', True)])
+    teacher_id = fields.Many2one('res.partner', string='Teacher', domain=[('is_teacher', '=', True)], default=_get_default_teacher)
     evaluation_ids = fields.One2many('grades.evaluation', 'course_id', string='Evaluations')
     student_ids = fields.Many2many('res.partner','grades_course_students_rel', string='Students')
     state = fields.Selection([('register', 'Register'),('in_progress', 'In Progress'), ('finished', 'Finished')], String = 'State', default='register')
